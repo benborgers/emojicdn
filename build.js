475db93e37e06e667eb3f818c046e1017334ae1c
benborgers <borgersbenjamin@gmail.com>
2019-01-30 23:51:16 -0500

    new fetching method that doesn't use puppeteer

--- build.js
+++ build.js
@@ -37,40 +37,65 @@ fs.readdirSync('src').forEach(file => {
     })
     
   const getEmoji = async url => {
-    const page = await browser.newPage();
-    await page.goto(url);
-    let emoji = await page.title();
-    emoji = emoji.split(' ')[0];
-    let imageUrl = await page.evaluate(`document.querySelector('.vendor-image').querySelector('img').srcset`);
-    imageUrl = imageUrl.split(' ')[0];
+    // const page = await browser.newPage();
+    // await page.goto(url);
+    // let emoji = await page.title();
+    // emoji = emoji.split(' ')[0];
+    // let imageUrl = await page.evaluate(`document.querySelector('.vendor-image').querySelector('img').srcset`);
+    // imageUrl = imageUrl.split(' ')[0];
+    // 
+    // await fetch(imageUrl)
+    //   .then(res => {
+    //     const dest = fs.createWriteStream(`site/${emoji}.png`);
+    //     res.body.pipe(dest);
+    // 
+    //     dest.on('finish', () => {
+    //       page.close();
+    //       emojiToFetch = emojiToFetch + 1;
+    //       console.log(`Downloaded ${emoji} (${emojiToFetch}/${emojiLinks.length})`)
+    // 
+    //       if(emojiToFetch < emojiLinks.length) {
+    //         getEmoji(emojiLinks[emojiToFetch]);
+    //       } else {
+    //         console.log(`Done! Site built.`);
+    //         process.exit();
+    //       }
+    //     })
+    //   })
     
-    await fetch(imageUrl)
+    fetch(url)
+      .then(res => res.text())
       .then(res => {
-        const dest = fs.createWriteStream(`site/${emoji}.png`);
-        res.body.pipe(dest);
+        const emoji = res.split('<title>')[1].split(' ')[0];
+        let imageUrl = /srcset="(?<imageUrl>.*?)"/.exec(res).groups.imageUrl;
         
-        dest.on('finish', () => {
-          page.close();
-          emojiToFetch = emojiToFetch + 1;
-          console.log(`Downloaded ${emoji} (${emojiToFetch}/${emojiLinks.length})`)
-          
-          if(emojiToFetch < emojiLinks.length) {
-            getEmoji(emojiLinks[emojiToFetch]);
-          } else {
-            console.log(`Done! Site built.`);
-            process.exit();
-          }
-        })
+        fetch(imageUrl)
+          .then(res => {
+            const dest = fs.createWriteStream(`site/${emoji}.png`);
+            res.body.pipe(dest);
+            
+            dest.on('finish', () => {
+              emojiToFetch = emojiToFetch + 1;
+              console.log(`Downloaded ${emoji} (${emojiToFetch}/${emojiLinks.length})`);
+              
+              if(emojiToFetch < emojiLinks.length) {
+                getEmoji(emojiLinks[emojiToFetch]);
+              } else {
+                console.log(`Done! Site built.`);
+                process.exit();
+              }
+            })
+          })
       })
   }
     
-  const browser = await puppeteer.launch({ headless: true });
+  // const browser = await puppeteer.launch({ headless: true });
   
   let emojiToFetch = 0;
   getEmoji(emojiLinks[emojiToFetch]);
   
-  for(let i=0; i<9; i++) {
-    emojiToFetch = emojiToFetch + 1;
-    getEmoji(emojiLinks[emojiToFetch]);
-  }
+  // for(let i=0; i<9; i++) {
+  //   emojiToFetch = emojiToFetch + 1;
+  //   getEmoji(emojiLinks[emojiToFetch]);
+  // }
 })()
