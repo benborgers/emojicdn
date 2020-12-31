9059321e366fcfef55dd67c11b2773c16af6b1a0
Ben Borgers <borgersbenjamin@gmail.com>
2020-12-30 19:38:52 -0500

    Cache for a year on Cloudflare

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmojiController;
 
 Route::get('/{emoji}', [EmojiController::class, 'show'])
-    ->middleware('cache.headers:public;max_age=0;s_maxage=86400');
+    ->middleware('cache.headers:public;max_age=0;s_maxage=31536000');
